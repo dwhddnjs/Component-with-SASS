32b841689079513a2a918153cf8ff4be708ba6b2
이종원 <per1215@naver.com>
2021-06-25 00:03:19 +0900

    make new button(small, medium, large)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <div className="buttons"></div>
+      <Button size="large">Button</Button>
       <Button>Button</Button>
+      <Button size="small">Button</Button>
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,13 @@
 import React from "react";
+import classNames from "classnames";
 import "./Button.scss";
 
 //size: large, medium, small
 export default function Button({ children, size }) {
-  return <button className={`Button ${size}`}>{children}</button>;
+  return <button className={classNames("Button", size)}>{children}</button>;
 }
+
+Button.defaultProps = {
+  size: "medium",
+};
+
