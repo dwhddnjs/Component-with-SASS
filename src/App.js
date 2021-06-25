7b4fe67c708f2ffdd29f8ace7eb7cc92ea46c2a5
이종원 <per1215@naver.com>
2021-06-25 23:56:23 +0900

    ...rest params

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Button>Button</Button>
         <Button size="small">Button</Button>
       </div>
+
       <div className="buttons">
         <Button color="gray" size="large">
           Button
@@ -19,6 +20,7 @@ function App() {
           Button
         </Button>
       </div>
+
       <div className="buttons">
         <Button color="pink" size="large">
           Button
@@ -42,13 +44,21 @@ function App() {
       </div>
 
       <div className="buttons">
-        <Button size="large" fullWidth>
+        <Button size="large" fullWidth className="customized-button">
           Button
         </Button>
         <Button color="gray" size="large" fullWidth>
           Button
         </Button>
-        <Button color="pink" size="large" fullWidth>
+        <Button
+          color="pink"
+          size="large"
+          fullWidth
+          onClick={() => console.log("버튼클릭")}
+          onMouseMove={() => {
+            console.log("마우스무브");
+          }}
+        >
           Button
         </Button>
       </div>
@@ -57,3 +67,8 @@ function App() {
 }
 
 export default App;
+
+// className 이 겹치지 않게 작성하는 팁
+// 1. 컴포넌트의 이름을 고유하게 지정
+// 2. 최상위 엘리먼트의 클래스이름을 컴포넌트 이름과 똑같게 
+// 3. 그내부에서 셀렉터를 사용
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,23 @@ import classNames from "classnames";
 import "./Button.scss";
 
 //size: large, medium, small
-export default function Button({ children, size, color, outline, fullWidth }) {
+export default function Button({
+  children,
+  size,
+  color,
+  outline,
+  fullWidth,
+  className,
+  ...rest
+}) {
   return (
     <button
       className={classNames("Button", size, color, {
         outline,
         fullWidth,
       })}
+      {...rest}
+      className
     >
       {children}
     </button>
